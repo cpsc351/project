05f530ebd634344ee74f54c6fc6cbbe921738102
sudo-update <seanjaviya@csu.fullerton.edu>
2021-08-02 22:33:18 -0700

    cleaned up some comments

--- consumer.cpp
+++ consumer.cpp
@@ -26,38 +26,28 @@ struct student {
     string endDate;
     string endTime;
 };
-//vector<student> stu;
 student* _stu;
-
 array<int, 4> myarray = {0, 0, 0, 0};
 int foundMinutes = 0;
 ofstream studentreport;
-
 int _n = 1;
 string _email;
-//student* _stu[] = &stu;
 static constexpr array<int, 4>* _rangeValues = &myarray;
 static constexpr int* _foundMinutes = &foundMinutes;
-//static string email;
 string email;
 
 
-
-
-int readTime(string time1) {
-  // time.erase(2, 1);
-  // int result = stoi(time);
-  // string endTime2 = endTime1;
-  string time2 = time1;
-  time1.erase(2, 3);
-  time2.erase(0, 3);
-  cout << time1 << ":" << time2 << "\n";
-  int time11 = stoi(time1);
-  int time22 = stoi(time2);
-  time11 = time11 * 60;
-  time22 += time11;
-  cout << time22 << " minutes\n";
-  return time22;
+int readTime(string hours) {
+  string minutes = hours;
+  hours.erase(2, 3);
+  minutes.erase(0, 3);
+  cout << hours << ":" << minutes << "\n";
+  int hoursInt = stoi(hours);
+  int minutesInt = stoi(minutes);
+  hoursInt = hoursInt * 60;
+  minutesInt += hoursInt;
+  cout << minutesInt << " minutes\n";
+  return minutesInt;
 }
 void* threaded_pass(void* arg) {
   int maxIndex = 0;
@@ -66,32 +56,18 @@ void* threaded_pass(void* arg) {
   minIndex = maxIndex - (_rangeValues->at(0));
   maxIndex--;
   int lastThreadChecker = _rangeValues->at(3);
-
   cout << lastThreadChecker;
   cout << " is the current thread compared to total of " << _rangeValues->at(2) << "\n";
-
   if (lastThreadChecker == _rangeValues->at(2)) {
     maxIndex += _rangeValues->at(1);
   }
   for (int j = minIndex; j <= maxIndex; j++) {
       if (_stu[j].email == email) {
-        // *_foundMinutes += readTime(_stu[j].endTime) - readTime(_stu[j].startTime);
         //studentReport.lock();
         studentReport.try_lock();
         int endTime1 = readTime(_stu[j].endTime);
-        // string endTime2 = endTime1;
-        // endTime1.erase(2, 2);
-        // endTime2.erase(0, 2);
-        // endTime1 = endTime1 * 60;
-        // endTime2 += endTime1;
         int startTime1 = readTime(_stu[j].startTime);
-        // int startTime2 = endTime1;
-        // startTime1.erase(2, 2);
-        // startTime2.erase(0, 2);
-        // startTime1 = startTime1 * 60;
-        // startTime2 += startTime1;
         *_foundMinutes += endTime1 - startTime1;
-
         studentreport << _stu[j].email << " " << _stu[j].endDate << " " << endTime1 - startTime1 << " minutes.\n";
         studentReport.unlock();
       }
@@ -103,9 +79,7 @@ void* threaded_pass(void* arg) {
     return 0;
 }
 int main(int argc, char* argv[]) {
-  //cout << "hello world";
   int shmid;
-  //cout << "next line works";
   key_t key = 886699586;
   void* shm;
   shmid = shmget(key, SHARESIZE, 0666);
@@ -118,7 +92,6 @@ int main(int argc, char* argv[]) {
     cout << "error in shmat\n";
     exit(1);
   }
-  //cout << "next line fails";
   //memcpy(shm, _stu, 6);                                  // hardcoded 6
   //(student*)copy = (student*) shm;
   student* shmCopy = (student*)shm;
@@ -137,7 +110,7 @@ int main(int argc, char* argv[]) {
   // int remainderRange = arraySize%_n;
   int baseRange = 6/_n;                                //hardcoded 6
   int remainderRange = 6%_n;                           //hardcoded 6
-
+  
   *_rangeValues = {baseRange, remainderRange, _n, 1};
   pthread_t threads[_n];
   studentreport.open("studentreport.txt");
@@ -149,7 +122,6 @@ int main(int argc, char* argv[]) {
   for (int i = 1; i <= _n; i++) {
     pthread_join(threads[i], NULL);
   }
-  //cout << _rangeValues->at(0) << _rangeValues->at(1) << _rangeValues->at(2) << _rangeValues->at(3) << " rangeValues\n";
   cout << *_foundMinutes << " minutes found.\n";
   studentreport << *_foundMinutes << " total minutes found.\n";
   studentreport.close();
--- producer.cpp
+++ producer.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-//#include <vector>
 #include <fstream>
 #include <string>
 #include <cstring>
@@ -14,7 +13,6 @@
 
 #define SHARESIZE 1000
 
-//#include "consumer.cpp"
 using namespace std;
 
 mutex zoomReport;
@@ -45,16 +43,10 @@ int main(int argc, char *argv[]) {
     cout << "error in shmat\n";
     exit(1);
   }
-
-  // consumer threaded(stoi(argv[3]), argv[2]);
   ifstream zoomreport;
-  //ofstream studentreport;
   string line;
   zoomReport.lock();
   zoomreport.open(argv[1]);
-  //studentReport.lock();
-  // studentreport.open("studentreport.txt");
-  //stuLock.lock();
   int number_of_lines = 0;
   while (getline(zoomreport, line)) {
     ++number_of_lines;
@@ -62,7 +54,6 @@ int main(int argc, char *argv[]) {
   zoomreport.close();
   zoomreport.open(argv[1]);
   student* stu = new student[number_of_lines];
-  //array<student, number_of_lines> stu;
   int i = 0;
   while(getline(zoomreport, line)) {
     parseText(line, stu, i);
@@ -74,22 +65,13 @@ int main(int argc, char *argv[]) {
     memcpy(shm, &(stu[i]), 64);
     cout << stu[i].email << " " << &stu[i] << "\n";
   }
-
   //memcpy(shm, stu, number_of_lines);
 
-  //stuLock.unlock();
   zoomreport.close();
   zoomReport.unlock();
-  // stuLock.lock();
-  // for(size_t i = 0; i < stu.size(); i++)
-  //   studentreport << stu[i].email << stu[i].startDate << stu[i].startTime << stu[i].endDate << stu[i].endTime << '\n';
-  // stuLock.unlock();
-  // studentreport.close();
-  // studentReport.unlock();
-  //threaded.execute();
   shmdt(shm);
 
-
+  //todo implement safely:
   //shmctl(shmid, IPC_RMID, NULL);
   return 0;
 }
@@ -103,15 +85,10 @@ void parseText(string line, student stu[], int index) {
         ssin >> arr[i];
         ++i;
     }
-
     studentStruct.email = arr[0];
     studentStruct.startDate = arr[1];
     studentStruct.startTime = arr[2];
     studentStruct.endDate = arr[3];
     studentStruct.endTime = arr[4];
-
-    //cout << "email: " << studentStruct.email << endl << "start date: " << studentStruct.startDate << endl << "start time: " << studentStruct.startTime << endl << "end date: " << studentStruct.endDate << endl << "end time: " << studentStruct.endTime << endl << endl;
-
     stu[index] = (studentStruct);
-    //cout << stu[index].email;
 }
