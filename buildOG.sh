dcd7b9957e28da904660966331bbb10fcfb85f61
sudo-update <70726692+sudo-update@users.noreply.github.com>
2021-08-03 19:47:15 -0700

    Delete buildOG.sh

--- buildOG.sh
+++
@@ -1,7 +0,0 @@
-# g++ -c consumer.hpp multi-threaded-consumer.o #-lpthread
-# g++ -c producer.cpp producer.o #-lpthread
-# g++ -o multi-threaded-consumer.o producer.o producer -lpthread
-# g++ -o multi-threaded-consumer.o multi-threaded-consumer -lpthread
-g++ -o producer consumer.hpp producer2.cpp -lpthread -std=c++17
-./producer $1 $2 $3
-#./multi-threaded-consumer $1
