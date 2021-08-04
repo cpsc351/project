9224fbea9f64f5730a5af303c028857412f5fbc8
sudo-update <seanjaviya@csu.fullerton.edu>
2021-08-03 21:35:08 -0700

    added documentation

--- consumer.cpp
+++ consumer.cpp
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <vector>
 #include <fstream>
 #include <string>
 #include <sstream>
@@ -18,25 +17,38 @@
 
 using namespace std;
 
-mutex studentReport, stuLock, rangeValuesLock;
+mutex studentReport, rangeValuesLock;
 
-int arraySize = 0;
 array<int, 4> myarray = {0, 0, 0, 0};
+int arraySize = 0;
 int foundMinutes = 0;
-ofstream studentreport;
-int _n = 1;
-string _email;
-static constexpr array<int, 4>* _rangeValues = &myarray;
-static constexpr int* _foundMinutes = &foundMinutes;
 string email;
+ofstream studentreport;
+int _n = 1;                                         // default value of 1 thread
+static constexpr int* _foundMinutes = &foundMinutes;
+//_rangeValues is used by each thread for the search algorithm and it has 4 values:
+//    the first value is the number of elements that each thread is responsible for searching through
+//    the second value is the number of elements that are unnacounted for during search
+//              these elements are searched through by the last thread
+//    the third value is the number of threads specified by the command line arguments
+//    the fourth value changes, and represents the current thread index
+static constexpr array<int, 4>* _rangeValues = &myarray;
 
+//helper function that takes military time (20:56) and converts into minutes since midnight
 int readTime(string time);
+//various date specific helper functions for extracting data from a line of the input text
 string getEndDate(string line);
 string getEmail(string line);
 string getStartTime(string line);
 string getEndTime(string line);
 
+//this function is executed by each thread.  the search algorithm searches an equal
+//amount of indices in each thread; the remaining indices are searched by the last thread.
+//the search is looking for email fields that match the email provided by command line arguments.
+//when a matching email is found, that email's matching session details are written to the report.
+//writing to the report is a critical section managed by a mutex.
 void* threaded_pass(void* arg) {
+  //this block determines which indices this thread is responsible for
   int maxIndex = 0;
   int minIndex = 0;
   string* _stu = (string*) arg;
@@ -44,14 +56,12 @@ void* threaded_pass(void* arg) {
   minIndex = maxIndex - (_rangeValues->at(0));
   maxIndex--;
   int lastThreadChecker = _rangeValues->at(3);
-  cout << lastThreadChecker;
-  cout << " is the current thread compared to total of " << _rangeValues->at(2) << "\n";
   if (lastThreadChecker == _rangeValues->at(2)) {
-    maxIndex += _rangeValues->at(1);
+    maxIndex += _rangeValues->at(1); //the last thread has extra searches sometimes
   }
+  //this block does the actual searching
   for (int j = minIndex; j <= maxIndex; j++) {
       if (getEmail(_stu[j]) == email) {
-        //studentReport.lock();
         studentReport.try_lock();
         int endTime1 = readTime(getEndTime(_stu[j]));
         int startTime1 = readTime(getStartTime(_stu[j]));
@@ -59,38 +69,38 @@ void* threaded_pass(void* arg) {
         studentreport << getEmail(_stu[j]) << " " << getEndDate(_stu[j]) << " " << endTime1 - startTime1 << " minutes.\n";
         studentReport.unlock();
       }
-      cout << "just searched array index = " << j << "\n";
-      cout << *_foundMinutes << " total minutes found.\n";
     }
-    (_rangeValues->at(3))++;
-    rangeValuesLock.unlock();
-    return 0;
+  (_rangeValues->at(3))++;
+  rangeValuesLock.unlock();
+  return 0;
 }
 
 int main(int argc, char* argv[]) {
   int shmid;
-  key_t key = 886699586;
-  void* shm;
+  key_t key = 886699586;                    // private key for the shared memory
+  void* shm;                       // pointer to shared memory, for writing into
   shmid = shmget(key, SHARESIZE, 0666);
   if (shmid < 0) {
-    cout << "error in shmget\n";
+    cout << "error in shmget\n";            //unable to create the shared memory
     exit(1);
   }
   shm = shmat(shmid, NULL, 0);
   if (shm == (char*)-1) {
-    cout << "error in shmat\n";
+    cout << "error in shmat\n";    //unable to get a handle on the shared memory
     exit(1);
   }
+  //reads the contents of shared memory and places it into a string (memoryReader)
   char* shmCopy;
   string memoryReader = "";
   int index = 0;
-  for (shmCopy = (char*)shm; *shmCopy != EOF; shmCopy++) {                    //works on hello world basic
-    //printf("%c", *shmCopy);                                        //works on hello world basic
+  for (shmCopy = (char*)shm; *shmCopy != EOF; shmCopy++) {
     memoryReader.push_back(*shmCopy);
     if (*shmCopy == '\n') {
       index++;
     }
-  }                                                           //works on hello world basic
+  }
+  // create a meaningful array from the input
+  // each element of the array will correspond to one line from the input file
   string inputArray[index];
   stringstream memoryRead(memoryReader);
   string line;
@@ -99,42 +109,49 @@ int main(int argc, char* argv[]) {
     inputArray[y] = line;
     y++;
   }
-  string (*_stu)[index];
+  string (*_stu)[index]; //we need a pointer handle to the array for the threads to accept it as a parameter
   _stu = &inputArray;
   arraySize = index;
   email = argv[1];
   _n = stoi(argv[2]);
+  // initialize _rangeValues for the threads to use when they conduct their searches
   int baseRange = arraySize/_n;
   int remainderRange = arraySize%_n;
   *_rangeValues = {baseRange, remainderRange, _n, 1};
   pthread_t threads[_n];
   studentreport.open("studentreport.txt");
-
+  // create n threads as specified by command line arguements
   for (int i = 1; i <= _n; i++) {
     rangeValuesLock.lock();
     pthread_create(&threads[i], NULL, threaded_pass, _stu);
   }
+  // join the threads after they finish their searches
   for (int i = 1; i <= _n; i++) {
     pthread_join(threads[i], NULL);
   }
-  cout << *_foundMinutes << " minutes found.\n";
+  // prints a total count of minutes found to the output device and to the report
+  cout << *_foundMinutes << " total minutes found.\n";
   studentreport << *_foundMinutes << " total minutes found.\n";
   studentreport.close();
-  shmdt(shm);
-  shmctl(shmid, IPC_RMID, NULL);
+  shmdt(shm);                                        //detach from shared memory
+  shmctl(shmid, IPC_RMID, NULL); //shared memory is no longer needed, so destroys shared memory
   return 0;
 }
 
-int readTime(string hours) {
+int readTime(string hours) {                    //input is military time (20:56)
   string minutes = hours;
-  hours.erase(2, 3);
-  minutes.erase(0, 3);
+  hours.erase(2, 3);                   // remove the ":" and everything after it
+  minutes.erase(0, 3);                // remove the ":" and everything before it
   int hoursInt = stoi(hours);
   int minutesInt = stoi(minutes);
   hoursInt = hoursInt * 60;
   minutesInt += hoursInt;
   return minutesInt;
 }
+
+// the following functions are data specific.
+// they won't work if the input text file does not meet the required format:
+//      email, start date, start time, end date, end time
 string getEmail(string line) {
   string delimiter = " ";
   string arr[5];
--- producer.cpp
+++ producer.cpp
@@ -1,13 +1,10 @@
-#include <thread>
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
 #include <cstring>
 #include <sstream>
-#include <mutex>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -16,44 +13,44 @@
 
 using namespace std;
 
-mutex zoomReport;
 
 int main(int argc, char *argv[]) {
   if (argc < 2) { cout << "Provide enough command line arguments"; }
   int shmid;
   key_t key;
-  void* shm;
-  key = 886699586;
+  void* shm;                       // pointer to shared memory, for writing into
+  key = 886699586;                          // private key for the shared memory
   shmid = shmget(key, SHARESIZE, IPC_CREAT | 0666);
   if (shmid < 0) {
-    cout << "error in shmget\n";
+    cout << "error in shmget\n";            //unable to create the shared memory
     exit(1);
   }
   shm = shmat(shmid, (void*)0, 0);
   if (shm == (char*)-1) {
-    cout << "error in shmat\n";
+    cout << "error in shmat\n";    //unable to get a handle on the shared memory
     exit(1);
   }
   ifstream zoomreport;
   string line;
-  zoomReport.lock();
-  zoomreport.open(argv[1]);
-  int number_of_lines = 0;
+  zoomreport.open(argv[1]); //opens the text file specified by the command line argument
+  int number_of_lines = 0;  //line count is needed for the construction of the array of strings
   while (getline(zoomreport, line)) {
     ++number_of_lines;
   }
   zoomreport.close();
   zoomreport.open(argv[1]);
-  string newStudent[number_of_lines];
+  string newStudent[number_of_lines]; //create a meaningful array from the input
+  // each element of the array will correspond to one line from the input file
   int i = 0;
   while(getline(zoomreport, line)) {
     newStudent[i] = line;
-    newStudent[i].push_back('\n'); //for parsing purposes when we retrieve
+    newStudent[i].push_back('\n'); //for delimiting purposes when we retrieve from shared memory
     i++;
   }
   char* pusher;
   pusher = (char*)shm;
   for (int i = 0; i < number_of_lines; i++) {
+    // create a cstring from each element of the array, then copy that cstring to shared memory
     char* cstr = new char[newStudent[i].length()];
     strcpy (cstr, newStudent[i].c_str());
     memcpy((void*)pusher, cstr, newStudent[i].length());
@@ -61,7 +58,6 @@ int main(int argc, char *argv[]) {
     delete[] cstr;
   }
   zoomreport.close();
-  zoomReport.unlock();
-  shmdt(shm);
+  shmdt(shm);                                        //detach from shared memory
   return 0;
 }
